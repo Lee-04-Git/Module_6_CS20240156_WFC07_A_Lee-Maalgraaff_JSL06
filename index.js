edba8405243fb3b91bd2a0cc0ed6069bca468a44
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-09-15 23:27:30 +0200

    create and append <ul> element for menu item listing

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ const menu = {
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
+    const menuContainer = document.getElementById('menu');
 
     // Loop through each category and its items in the menu object
+    const categories = Object.keys(menu);
+    for (let i = 0; i < categories.length; i++) {
+
+        // Get the current category name
+        const categoryName = categories[i];
 
         // Create an element to represent the category
-
+        const categoryElement = document.createElement('h3');
+        
         // Set the text content of the category element to the category name
+        categoryElement.textContent = categoryName;
 
         // Append the category element to the menu container
+        menuContainer.appendChild(categoryElement);
 
         // Create an element to represent a list of items
+        const itemsList = document.createElement('ul');
 
         // Append a list of items element to the menu container
+        menuContainer.appendChild(itemsList);
 
         // Loop through the items in the category and create list items
 
@@ -30,7 +41,7 @@ function displayMenuItems(menu) {
             // Attach a click event listener to the list item to add it to the order
 
             // Append the list item to the list of items
-
+    }
             
 }
 
